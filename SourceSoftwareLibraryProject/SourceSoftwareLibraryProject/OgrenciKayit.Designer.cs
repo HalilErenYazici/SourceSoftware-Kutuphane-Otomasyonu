ddbbca7baebacf0b7938c96693764c6bb288b39f
kadiirkorkmaz <102298123+kadiirkorkmaz@users.noreply.github.com>
2022-04-23 18:22:05 +0300

    Öğrenci Kayıt Ekranı
    23.04.2022
    18.20
    
    Kart Id Düzenlenecek
    E-posta Sınırlama Getirelecek
    Başka Bir Eksik Varsa Göz Atılacak

--- SourceSoftwareLibraryProject/SourceSoftwareLibraryProject/OgrenciKayit.Designer.cs
+++ SourceSoftwareLibraryProject/SourceSoftwareLibraryProject/OgrenciKayit.Designer.cs
@@ -29,12 +29,285 @@ namespace SourceSoftwareLibraryProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtKartId = new System.Windows.Forms.TextBox();
+            this.lblKartId = new System.Windows.Forms.Label();
+            this.lblOgrNo = new System.Windows.Forms.Label();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.lblEposta = new System.Windows.Forms.Label();
+            this.lblBolum = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtEposta = new System.Windows.Forms.TextBox();
+            this.cmbBolum = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.mskTel = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mskOgrNo = new System.Windows.Forms.MaskedTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCinsiyet = new System.Windows.Forms.Label();
+            this.radioErkek = new System.Windows.Forms.RadioButton();
+            this.radioKadın = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtKartId
+            // 
+            this.txtKartId.Location = new System.Drawing.Point(172, 98);
+            this.txtKartId.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtKartId.Name = "txtKartId";
+            this.txtKartId.Size = new System.Drawing.Size(160, 37);
+            this.txtKartId.TabIndex = 0;
+            // 
+            // lblKartId
+            // 
+            this.lblKartId.AutoSize = true;
+            this.lblKartId.Location = new System.Drawing.Point(73, 105);
+            this.lblKartId.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblKartId.Name = "lblKartId";
+            this.lblKartId.Size = new System.Drawing.Size(85, 30);
+            this.lblKartId.TabIndex = 1;
+            this.lblKartId.Text = "Kart Id:";
+            // 
+            // lblOgrNo
+            // 
+            this.lblOgrNo.AutoSize = true;
+            this.lblOgrNo.Location = new System.Drawing.Point(23, 161);
+            this.lblOgrNo.Name = "lblOgrNo";
+            this.lblOgrNo.Size = new System.Drawing.Size(135, 30);
+            this.lblOgrNo.TabIndex = 2;
+            this.lblOgrNo.Text = "Öğrenci No:";
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(111, 221);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(47, 30);
+            this.lblAd.TabIndex = 3;
+            this.lblAd.Text = "Ad:";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(78, 277);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(80, 30);
+            this.lblSoyad.TabIndex = 4;
+            this.lblSoyad.Text = "Soyad:";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(65, 383);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(93, 30);
+            this.lblTelefon.TabIndex = 5;
+            this.lblTelefon.Text = "Telefon:";
+            // 
+            // lblEposta
+            // 
+            this.lblEposta.AutoSize = true;
+            this.lblEposta.Location = new System.Drawing.Point(66, 440);
+            this.lblEposta.Name = "lblEposta";
+            this.lblEposta.Size = new System.Drawing.Size(92, 30);
+            this.lblEposta.TabIndex = 6;
+            this.lblEposta.Text = "E Posta:";
+            // 
+            // lblBolum
+            // 
+            this.lblBolum.AutoSize = true;
+            this.lblBolum.Location = new System.Drawing.Point(75, 487);
+            this.lblBolum.Name = "lblBolum";
+            this.lblBolum.Size = new System.Drawing.Size(83, 30);
+            this.lblBolum.TabIndex = 7;
+            this.lblBolum.Text = "Bölüm:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(172, 214);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(160, 37);
+            this.txtAd.TabIndex = 9;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(172, 270);
+            this.txtSoyad.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(160, 37);
+            this.txtSoyad.TabIndex = 10;
+            // 
+            // txtEposta
+            // 
+            this.txtEposta.Location = new System.Drawing.Point(172, 433);
+            this.txtEposta.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtEposta.Name = "txtEposta";
+            this.txtEposta.Size = new System.Drawing.Size(160, 37);
+            this.txtEposta.TabIndex = 12;
+            // 
+            // cmbBolum
+            // 
+            this.cmbBolum.FormattingEnabled = true;
+            this.cmbBolum.Items.AddRange(new object[] {
+            "Inegol Isletme Fakultesi",
+            "Inegol MYO"});
+            this.cmbBolum.Location = new System.Drawing.Point(172, 484);
+            this.cmbBolum.Name = "cmbBolum";
+            this.cmbBolum.Size = new System.Drawing.Size(160, 38);
+            this.cmbBolum.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe Print", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(309, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(389, 59);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Öğrenci Kayıt Ekranı";
+            // 
+            // mskTel
+            // 
+            this.mskTel.Location = new System.Drawing.Point(172, 376);
+            this.mskTel.Mask = "(999) 000-0000";
+            this.mskTel.Name = "mskTel";
+            this.mskTel.Size = new System.Drawing.Size(160, 37);
+            this.mskTel.TabIndex = 15;
+            this.mskTel.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mskTel_MaskInputRejected);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 528);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 47);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Kayıt Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mskOgrNo
+            // 
+            this.mskOgrNo.Location = new System.Drawing.Point(172, 154);
+            this.mskOgrNo.Mask = "000000000";
+            this.mskOgrNo.Name = "mskOgrNo";
+            this.mskOgrNo.Size = new System.Drawing.Size(160, 37);
+            this.mskOgrNo.TabIndex = 17;
+            this.mskOgrNo.ValidatingType = typeof(int);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioKadın);
+            this.panel1.Controls.Add(this.radioErkek);
+            this.panel1.Location = new System.Drawing.Point(172, 316);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 41);
+            this.panel1.TabIndex = 18;
+            // 
+            // lblCinsiyet
+            // 
+            this.lblCinsiyet.AutoSize = true;
+            this.lblCinsiyet.Location = new System.Drawing.Point(60, 326);
+            this.lblCinsiyet.Name = "lblCinsiyet";
+            this.lblCinsiyet.Size = new System.Drawing.Size(98, 30);
+            this.lblCinsiyet.TabIndex = 19;
+            this.lblCinsiyet.Text = "Cinsiyet:";
+            // 
+            // radioErkek
+            // 
+            this.radioErkek.AutoSize = true;
+            this.radioErkek.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioErkek.Location = new System.Drawing.Point(6, 10);
+            this.radioErkek.Name = "radioErkek";
+            this.radioErkek.Size = new System.Drawing.Size(71, 26);
+            this.radioErkek.TabIndex = 0;
+            this.radioErkek.TabStop = true;
+            this.radioErkek.Text = "Erkek";
+            this.radioErkek.UseVisualStyleBackColor = true;
+            // 
+            // radioKadın
+            // 
+            this.radioKadın.AutoSize = true;
+            this.radioKadın.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioKadın.Location = new System.Drawing.Point(83, 10);
+            this.radioKadın.Name = "radioKadın";
+            this.radioKadın.Size = new System.Drawing.Size(74, 26);
+            this.radioKadın.TabIndex = 1;
+            this.radioKadın.TabStop = true;
+            this.radioKadın.Text = "Kadın";
+            this.radioKadın.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 545);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 30);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // OgrenciKayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(996, 583);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCinsiyet);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.mskOgrNo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mskTel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmbBolum);
+            this.Controls.Add(this.txtEposta);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblBolum);
+            this.Controls.Add(this.lblEposta);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.lblOgrNo);
+            this.Controls.Add(this.lblKartId);
+            this.Controls.Add(this.txtKartId);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "OgrenciKayit";
             this.Text = "OgrenciKayit";
+            this.Load += new System.EventHandler(this.OgrenciKayit_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtKartId;
+        private System.Windows.Forms.Label lblKartId;
+        private System.Windows.Forms.Label lblOgrNo;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.Label lblEposta;
+        private System.Windows.Forms.Label lblBolum;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtEposta;
+        private System.Windows.Forms.ComboBox cmbBolum;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox mskTel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox mskOgrNo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioKadın;
+        private System.Windows.Forms.RadioButton radioErkek;
+        private System.Windows.Forms.Label lblCinsiyet;
+        private System.Windows.Forms.Label label1;
     }
 }
--- SourceSoftwareLibraryProject/SourceSoftwareLibraryProject/OgrenciKayit.cs
+++ SourceSoftwareLibraryProject/SourceSoftwareLibraryProject/OgrenciKayit.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace SourceSoftwareLibraryProject
 {
@@ -15,6 +17,70 @@ namespace SourceSoftwareLibraryProject
         public OgrenciKayit()
         {
             InitializeComponent();
+        }
+
+        private void OgrenciKayit_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mskTel_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (mskTel.Text.Length !=14)
+            {
+                MessageBox.Show("Telefon numarası 11 hanenin altında olamaz!");
+            }
+
+            if(mskOgrNo.Text.Length != 9)
+            {
+                MessageBox.Show("Öğrenci Numarası 9 hanenin altında olamaz!");
+            }
+             
+            if(txtAd.Text.Length < 2)
+            {
+                MessageBox.Show("Adınızı Giriniz!");
+            }
+
+            if(txtSoyad.Text.Length < 2)
+            {
+                MessageBox.Show("Soyadınızı Giriniz!");
+            }
+
+
+            if (radioErkek.Checked)
+            {
+
+                label1.Text = "Cinsiyet" + radioErkek.Text;
+            }
+            else if (radioKadın.Checked)
+            {
+                
+                label1.Text = "Cinsiyet" + radioKadın.Text;
+            }
+            else
+            {
+                MessageBox.Show("Lüften cinsiyet seçiniz!");
+
+            }
+
+            if (txtEposta.Text == "" || txtEposta.Text.Length < 4)
+            {
+                MessageBox.Show("Lütfen Geçerli Bir E-Posta Giriniz");
+            }
+
+
+
+            if (cmbBolum.Text == "")
+            {
+                MessageBox.Show("Boş Geçilemez");
+            }
+
+
         }
     }
 }
