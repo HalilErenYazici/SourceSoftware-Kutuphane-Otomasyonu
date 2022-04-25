741134363ae1be92733f65d18f77f6ecf4445af7
Baha-Can <102296363+Baha-Can@users.noreply.github.com>
2022-04-25 20:47:02 +0300

    Anasayfa Genel Tasarımı
    25.04.2022
    20.47
    -------------------------------------------
    Anasayfadan diğer sayfalara geçiş için butonlar konuldu.

--- SourceSoftwareLibraryProject/SourceSoftwareLibraryProject/AnaSayfa.Designer.cs
+++ SourceSoftwareLibraryProject/SourceSoftwareLibraryProject/AnaSayfa.Designer.cs
@@ -29,32 +29,188 @@ namespace SourceSoftwareLibraryProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnEmanetAlmaVerme = new System.Windows.Forms.Button();
+            this.btnEmanetGoruntuleme = new System.Windows.Forms.Button();
+            this.btnEmegiGecenler = new System.Windows.Forms.Button();
+            this.btnGirisSayfasi = new System.Windows.Forms.Button();
+            this.btnKitapDuzenleme = new System.Windows.Forms.Button();
+            this.btnKitapEkleme = new System.Windows.Forms.Button();
+            this.btnKitapGoruntuleme = new System.Windows.Forms.Button();
+            this.btnOgrenciDuzenleme = new System.Windows.Forms.Button();
+            this.btnOgrenciIsleri = new System.Windows.Forms.Button();
+            this.btnOgrenciKayit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btnEmanetAlmaVerme
             // 
-            this.button1.Location = new System.Drawing.Point(80, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEmanetAlmaVerme.ImageKey = "(none)";
+            this.btnEmanetAlmaVerme.Location = new System.Drawing.Point(3, 67);
+            this.btnEmanetAlmaVerme.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEmanetAlmaVerme.Name = "btnEmanetAlmaVerme";
+            this.btnEmanetAlmaVerme.Size = new System.Drawing.Size(268, 48);
+            this.btnEmanetAlmaVerme.TabIndex = 0;
+            this.btnEmanetAlmaVerme.Text = "Emanet Alma Verme";
+            this.btnEmanetAlmaVerme.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEmanetAlmaVerme.UseVisualStyleBackColor = true;
+            this.btnEmanetAlmaVerme.Click += new System.EventHandler(this.btnEmanetAlmaVerme_Click);
+            // 
+            // btnEmanetGoruntuleme
+            // 
+            this.btnEmanetGoruntuleme.Location = new System.Drawing.Point(3, 116);
+            this.btnEmanetGoruntuleme.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEmanetGoruntuleme.Name = "btnEmanetGoruntuleme";
+            this.btnEmanetGoruntuleme.Size = new System.Drawing.Size(268, 48);
+            this.btnEmanetGoruntuleme.TabIndex = 1;
+            this.btnEmanetGoruntuleme.Text = "Emanet Görüntüleme";
+            this.btnEmanetGoruntuleme.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEmanetGoruntuleme.UseVisualStyleBackColor = true;
+            this.btnEmanetGoruntuleme.Click += new System.EventHandler(this.btnEmanetGoruntuleme_Click);
+            // 
+            // btnEmegiGecenler
+            // 
+            this.btnEmegiGecenler.Location = new System.Drawing.Point(3, 165);
+            this.btnEmegiGecenler.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEmegiGecenler.Name = "btnEmegiGecenler";
+            this.btnEmegiGecenler.Size = new System.Drawing.Size(268, 48);
+            this.btnEmegiGecenler.TabIndex = 2;
+            this.btnEmegiGecenler.Text = "Emeği Geçenler";
+            this.btnEmegiGecenler.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEmegiGecenler.UseVisualStyleBackColor = true;
+            this.btnEmegiGecenler.Click += new System.EventHandler(this.btnEmegiGecenler_Click);
+            // 
+            // btnGirisSayfasi
+            // 
+            this.btnGirisSayfasi.Location = new System.Drawing.Point(3, 214);
+            this.btnGirisSayfasi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGirisSayfasi.Name = "btnGirisSayfasi";
+            this.btnGirisSayfasi.Size = new System.Drawing.Size(268, 48);
+            this.btnGirisSayfasi.TabIndex = 3;
+            this.btnGirisSayfasi.Text = "Giriş Sayfası";
+            this.btnGirisSayfasi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGirisSayfasi.UseVisualStyleBackColor = true;
+            this.btnGirisSayfasi.Click += new System.EventHandler(this.btnGirisSayfasi_Click);
+            // 
+            // btnKitapDuzenleme
+            // 
+            this.btnKitapDuzenleme.Location = new System.Drawing.Point(3, 263);
+            this.btnKitapDuzenleme.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKitapDuzenleme.Name = "btnKitapDuzenleme";
+            this.btnKitapDuzenleme.Size = new System.Drawing.Size(268, 48);
+            this.btnKitapDuzenleme.TabIndex = 4;
+            this.btnKitapDuzenleme.Text = "Kitap Düzenleme";
+            this.btnKitapDuzenleme.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnKitapDuzenleme.UseVisualStyleBackColor = true;
+            this.btnKitapDuzenleme.Click += new System.EventHandler(this.btnKitapDuzenleme_Click);
+            // 
+            // btnKitapEkleme
+            // 
+            this.btnKitapEkleme.Location = new System.Drawing.Point(3, 312);
+            this.btnKitapEkleme.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKitapEkleme.Name = "btnKitapEkleme";
+            this.btnKitapEkleme.Size = new System.Drawing.Size(268, 48);
+            this.btnKitapEkleme.TabIndex = 5;
+            this.btnKitapEkleme.Text = "Kitap Ekleme";
+            this.btnKitapEkleme.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnKitapEkleme.UseVisualStyleBackColor = true;
+            this.btnKitapEkleme.Click += new System.EventHandler(this.btnKitapEkleme_Click);
+            // 
+            // btnKitapGoruntuleme
+            // 
+            this.btnKitapGoruntuleme.Location = new System.Drawing.Point(3, 361);
+            this.btnKitapGoruntuleme.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKitapGoruntuleme.Name = "btnKitapGoruntuleme";
+            this.btnKitapGoruntuleme.Size = new System.Drawing.Size(268, 48);
+            this.btnKitapGoruntuleme.TabIndex = 6;
+            this.btnKitapGoruntuleme.Text = "Kitapları Görüntüleme";
+            this.btnKitapGoruntuleme.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnKitapGoruntuleme.UseVisualStyleBackColor = true;
+            this.btnKitapGoruntuleme.Click += new System.EventHandler(this.btnKitapGoruntuleme_Click);
+            // 
+            // btnOgrenciDuzenleme
+            // 
+            this.btnOgrenciDuzenleme.Location = new System.Drawing.Point(3, 410);
+            this.btnOgrenciDuzenleme.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOgrenciDuzenleme.Name = "btnOgrenciDuzenleme";
+            this.btnOgrenciDuzenleme.Size = new System.Drawing.Size(268, 48);
+            this.btnOgrenciDuzenleme.TabIndex = 7;
+            this.btnOgrenciDuzenleme.Text = "Öğrenci Düzenleme";
+            this.btnOgrenciDuzenleme.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOgrenciDuzenleme.UseVisualStyleBackColor = true;
+            this.btnOgrenciDuzenleme.Click += new System.EventHandler(this.btnOgrenciDuzenleme_Click);
+            // 
+            // btnOgrenciIsleri
+            // 
+            this.btnOgrenciIsleri.Location = new System.Drawing.Point(3, 459);
+            this.btnOgrenciIsleri.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOgrenciIsleri.Name = "btnOgrenciIsleri";
+            this.btnOgrenciIsleri.Size = new System.Drawing.Size(268, 48);
+            this.btnOgrenciIsleri.TabIndex = 8;
+            this.btnOgrenciIsleri.Text = "Öğrenci İşleri";
+            this.btnOgrenciIsleri.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOgrenciIsleri.UseVisualStyleBackColor = true;
+            this.btnOgrenciIsleri.Click += new System.EventHandler(this.btnOgrenciIsleri_Click);
+            // 
+            // btnOgrenciKayit
+            // 
+            this.btnOgrenciKayit.Location = new System.Drawing.Point(3, 508);
+            this.btnOgrenciKayit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOgrenciKayit.Name = "btnOgrenciKayit";
+            this.btnOgrenciKayit.Size = new System.Drawing.Size(268, 48);
+            this.btnOgrenciKayit.TabIndex = 9;
+            this.btnOgrenciKayit.Text = "Öğrenci Kayıt";
+            this.btnOgrenciKayit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOgrenciKayit.UseVisualStyleBackColor = true;
+            this.btnOgrenciKayit.Click += new System.EventHandler(this.btnOgrenciKayit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(46, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 43);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Anasayfa";
             // 
             // AnaSayfa
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1000, 560);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOgrenciKayit);
+            this.Controls.Add(this.btnOgrenciIsleri);
+            this.Controls.Add(this.btnOgrenciDuzenleme);
+            this.Controls.Add(this.btnKitapGoruntuleme);
+            this.Controls.Add(this.btnKitapEkleme);
+            this.Controls.Add(this.btnKitapDuzenleme);
+            this.Controls.Add(this.btnGirisSayfasi);
+            this.Controls.Add(this.btnEmegiGecenler);
+            this.Controls.Add(this.btnEmanetGoruntuleme);
+            this.Controls.Add(this.btnEmanetAlmaVerme);
+            this.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AnaSayfa";
             this.Text = "AnaSayfa";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEmanetAlmaVerme;
+        private System.Windows.Forms.Button btnEmanetGoruntuleme;
+        private System.Windows.Forms.Button btnEmegiGecenler;
+        private System.Windows.Forms.Button btnGirisSayfasi;
+        private System.Windows.Forms.Button btnKitapDuzenleme;
+        private System.Windows.Forms.Button btnKitapEkleme;
+        private System.Windows.Forms.Button btnKitapGoruntuleme;
+        private System.Windows.Forms.Button btnOgrenciDuzenleme;
+        private System.Windows.Forms.Button btnOgrenciIsleri;
+        private System.Windows.Forms.Button btnOgrenciKayit;
+        private System.Windows.Forms.Label label1;
     }
 }
--- SourceSoftwareLibraryProject/SourceSoftwareLibraryProject/AnaSayfa.cs
+++ SourceSoftwareLibraryProject/SourceSoftwareLibraryProject/AnaSayfa.cs
@@ -16,5 +16,75 @@ namespace SourceSoftwareLibraryProject
         {
             InitializeComponent();
         }
+
+        private void btnEmanetAlmaVerme_Click(object sender, EventArgs e)
+        {
+            EmanetAlmaVermeSayfasi yeni = new EmanetAlmaVermeSayfasi();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnEmanetGoruntuleme_Click(object sender, EventArgs e)
+        {
+            EmanetGoruntulemeSayfasi yeni = new EmanetGoruntulemeSayfasi();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnEmegiGecenler_Click(object sender, EventArgs e)
+        {
+            EmegiGecenler yeni = new EmegiGecenler();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnGirisSayfasi_Click(object sender, EventArgs e)
+        {
+            GirisSayfasi yeni = new GirisSayfasi();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnKitapDuzenleme_Click(object sender, EventArgs e)
+        {
+            KitapDuzenle yeni = new KitapDuzenle();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnKitapEkleme_Click(object sender, EventArgs e)
+        {
+            KitapEkle yeni = new KitapEkle();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnKitapGoruntuleme_Click(object sender, EventArgs e)
+        {
+            KitaplariGoruntule yeni = new KitaplariGoruntule();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnOgrenciDuzenleme_Click(object sender, EventArgs e)
+        {
+            OgrenciDuzenleme yeni = new OgrenciDuzenleme();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnOgrenciIsleri_Click(object sender, EventArgs e)
+        {
+            OgrenciIsleriSayfasi yeni = new OgrenciIsleriSayfasi();
+            yeni.Show();
+            this.Hide();
+        }
+
+        private void btnOgrenciKayit_Click(object sender, EventArgs e)
+        {
+            OgrenciKayit yeni = new OgrenciKayit();
+            yeni.Show();
+            this.Hide();
+        }
     }
 }
